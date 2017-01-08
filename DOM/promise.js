03f8226a8394c69caf0104bad88fd2a94b0ecf56
mr_wolverine <wei1157@hotmail.com>
2017-01-08 11:11:28 +0800

    Promise: Serial asynchronous operation

--- DOM/promise.js
+++ DOM/promise.js
@@ -1,7 +1,7 @@
 'use strtic'
 /**
  * Promise 链式调用
- */
+ *
 new Promise(function (resolve, reject) {
     console.log('start new Promise...');
     var timeOut = Math.random() * 2;
@@ -25,6 +25,32 @@ new Promise(function (resolve, reject) {
  * Promise 串行异步操作
  * Promise.then().then().then()....
  */
+function multiply(input) {
+    return new Promise(function (resolve, reject) {
+        console.log('calculating ' + input + ' X ' + input + '...');
+        setTimeout(resolve, 500, input * input);
+    });
+}
+
+function add(input) {
+    return new Promise(function (resolve, reject) {
+        console.log('calculating ' + input + ' + ' + input + '...');
+        setTimeout(resolve, 500, input + input);
+    });
+}
+
+var p = new Promise(function (resolve, reject) {
+    console.log('start new Promise...');
+    resolve(123);
+});
+
+p.then(multiply)
+    .then(add)
+    .then(multiply)
+    .then(add)
+    .then(function(result){
+        console.log('Got value: ' + result);
+    });
 
 /**
  * Promise 并行异步操作
